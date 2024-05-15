df06a43a935a69457c039de814d6013620b60dfb
Iman Kamyabizadeh <ikamyabizadeh@myseneca.ca>
2024-05-14 22:26:57 -0400

    submmit lab2

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Iman Kamyabizadeh
+# Author ID: 150450229
+# Date Created: 2024/5/14
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
